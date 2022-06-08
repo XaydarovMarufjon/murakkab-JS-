19465cfcf2edf7107c8a2197a4ed2187e8a5c9e6
Sarvar170803 <sarvarazamatov07062001@gmail.com>
2022-06-08 10:03:04 +0500

    yangi update

---
+++ Murakkab Java Script/JsHint/array.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// const arr = [2,3,4,5,6];
+
+// arr.forEach( (item , index , arr)=>{
+//     console.log(`${index}: ${item} in to ${arr}`);
+// })
+
+
+//split
+// const movies = prompt('whats your favoruite movies' , '');
+// const usermovies =movies.split(',');
+// console.log(usermovies);
+
+// sort 
+// const arr2= [5,345,37,37342,6]
+// arr2.sort((a,b)=>a-b);  // sonlarni osish boycha sortlash 
+// console.log(arr2);
+
+/////  clone =>  objectni ichidagi malumotlarmni kochirish  
+
+const numbers =  { 
+    x:10 , 
+    y:2,
+    z:{
+        a:1,
+        b:2
+    }
+}
+const newNumbers = Object.assign({} , numbers)
+newNumbers.x = 20 
+console.log(newNumbers);
+
+
+
+
+
+
+
+
+
